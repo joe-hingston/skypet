076b1d7d5d6a39da2aa5bb8490ae88cb69192249
aaron <afletcher53@gmail.com>
2019-03-07 20:29:37 +0000

    Hey biatch

--- app/OutputFetcher.php
+++ app/OutputFetcher.php
@@ -4,7 +4,9 @@
 namespace App;
 
 
+use App\Jobs\ProcessOutput;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Log;
 
 class OutputFetcher
 {
@@ -14,21 +16,23 @@ class OutputFetcher
 
     public $total;
 
-    public $qty = 100;
+    public $qty = 1000;
 
     public $offset = 0;
 
     public $issn;
 
-    public function __construct(Client $client, $issn)
+    public $records;
+
+    public $journalID;
+
+    public function __construct(Client $client, Journal $journal)
     {
         $this->client = $client;
-        $this->issn = $issn;
-
-        $this->apiUri = $this->setApiUri('http://api.crossref.org/works?filter=issn:'. $issn);
-
+        $this->journal = $journal;
+        $this->apiUri = 'http://api.crossref.org/works?filter=issn:'. $journal->issn;
         $this->setTotal(
-            json_decode($this->client->request('GET', $this->apiUri)->getBody())->message->{'total-results'}
+            json_decode($this->client->request('GET', $this->buildURL())->getBody())->message->{'total-results'}
         );
     }
 
@@ -39,20 +43,41 @@ class OutputFetcher
      */
     public function fetch($records = [])
     {
+
+
+
         while ($this->offset < $this->total) {
-
             $res = $this->client->request('GET', $this->buildUrl());
-
             $decoded_items = json_decode($res->getBody())->message->items;
-
             foreach ($decoded_items as $item) {
 
-                $return[] = ['doi' => $item->DOI];
 
-            }
+            if (isset($item->title)) { // Filter out the ones with no titles
+                $output = Output::updateorCreate(['doi' => $item->DOI],
+                    [
+                        'doi'=> $item->DOI,
+                        'journal_id' => $this->journalID,
+                        'reference_count' => $item->{'reference-count'},
+                        'url'=>$item->URL,
+                        'title'=>$item->title[0],
+                        'issn'=>$item->ISSN[0],
+                        'publisher'=>$item->publisher,
+                        'language'=>$item->language,
+                        'is_referenced_by'=>$item->{'is-referenced-by-count'}
+
+                    ] );
+
+                //Post processing
+                ProcessOutput::dispatch($output, $item->DOI)->onConnection('redis'); //grab the abstract
+
+            }   };
+
+
 
             $this->offset += $this->qty;
         }
+;
+
 
         return $records;
     }
@@ -62,6 +87,7 @@ class OutputFetcher
      */
     public function buildUrl()
     {
+
         return $this->apiUri . http_build_query([
             'offset' => $this->offset,
             'rows' => $this->qty
@@ -74,6 +100,8 @@ class OutputFetcher
      */
     public function setApiUri($url)
     {
+
+
         $this->apiUri = $url;
     }
 
