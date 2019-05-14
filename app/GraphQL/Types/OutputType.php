c73f6a04f483c689dfe08edd36e48439d4e1c5e0
vagrant <vagrant@homestead>
2019-05-14 20:57:28 +0000

    added more fields ot the OutputType GraphQL

--- app/GraphQL/Types/OutputType.php
+++ app/GraphQL/Types/OutputType.php
@@ -27,9 +27,79 @@ class OutputType extends GraphQLType
                 'type' => Type::string(),
                 'description' => 'Abstract of the Output',
             ],
+            'doi' => [
+                'type' => Type::string(),
+                'description' => 'DOI of the output',
+            ],
+            'publisher' => [
+                'type' => Type::string(),
+                'description' => 'Publisher of the output',
+            ],
+            'issn' => [
+                'type' => Type::string(),
+                'description' => 'ISSN for the Output',
+            ],
+            'eissn' => [
+                'type' => Type::string(),
+                'description' => 'EISSN for the Output',
+            ],
+            'language' => [
+                'type' => Type::string(),
+                'description' => 'Language of the Output',
+            ],
+            'reference_count' => [
+                'type' => Type::string(),
+                'description' => 'reference count of the Output',
+            ],
+            'page' => [
+                'type' => Type::string(),
+                'description' => 'Page of the Output',
+            ],
+            'url' => [
+                'type' => Type::string(),
+                'description' => 'Url of the Output',
+            ],
+            'miningurl' => [
+                'type' => Type::string(),
+                'description' => 'MiningUrl of the Output',
+            ],
+            'is_referenced_by' => [
+                'type' => Type::string(),
+                'description' => 'How many is referenced Output',
+            ],
+            'source' => [
+                'type' => Type::string(),
+                'description' => 'Source of the Output',
+            ],
+            'prefix' => [
+                 'type' => Type::string(),
+                'description' => 'Prefix of the Output',
+            ],
+            'volume' => [
+                'type' => Type::string(),
+                'description' => 'Volume of the Output',
+            ],
+            'member' => [
+                'type' => Type::string(),
+                'description' => 'Volume of the Output',
+            ],
+            'score' => [
+                'type' => Type::string(),
+                'description' => 'Volume of the Output',
+            ],
+            'issue' => [
+                'type' => Type::string(),
+                'description' => 'Volume of the Output',
+            ],
 
+            //TODO change the fields so they don't contain dashes - Cannot use hyphens in GraphQL
 
         ];
+
+        // protected function resolveCreatedAtField($root, $args)
+        //      {
+        //        return (string) $root->created_at;
+        //      }
     }
 
 }
--- config/graphql.php
+++ config/graphql.php
@@ -1,7 +1,7 @@
 <?php
 
 
-
+use App\GraphQL\Types\OutputType;
 
 return [
 
